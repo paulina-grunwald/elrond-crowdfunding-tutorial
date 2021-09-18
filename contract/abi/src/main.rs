9e77e97652bcaf704f6d0811028b2f6b9768029f
Paulina Grunwald <paulina.grunwald@gmail.com>
2021-09-18 19:48:11 +0200

    add basic contract

---
+++ contract/abi/src/main.rs
@@ -0,0 +1,7 @@
+use contract::*;
+use elrond_wasm_debug::*;
+
+fn main() {
+	let contract = CrowdfundingImpl::new(TxContext::dummy());
+	print!("{}", abi_json::contract_abi(&contract));
+}
---
+++ mycontract/wasm/src/lib.rs
@@ -0,0 +1,5 @@
+
+#![no_std]
+
+pub use mycontract::*;
+pub use elrond_wasm_output::*;
